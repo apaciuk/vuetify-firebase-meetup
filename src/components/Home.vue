cef5250c5d1309ab999611ffdd0712ad457c77d0
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-05 21:08:57 +0000

    Update

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+ <main>
+ <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <v-layout column align-center>
+            <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
+            <blockquote>
+              &#8220;First, solve the problem. Then, write the code. And dont forget your toothbrush&#8221;
+              <footer>
+                <small>
+                  <em>&mdash;Batman</em>
+                </small>
+              </footer>
+            </blockquote>
+             <img src="/static/github.webp" alt="Github" class="mb-5" width="400px" />
+          </v-layout>
+        </v-slide-y-transition>
+      </v-container>
+    </main>
+</template>
