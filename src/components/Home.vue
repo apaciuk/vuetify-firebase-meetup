11f53307f5c2b437deece7e623175f3ae2b6520e
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-05 22:22:51 +0000

    Update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,36 @@
 <template>
- <main>
- <v-container fluid>
-        <v-slide-y-transition mode="out-in">
-          <v-layout column align-center>
-            <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code. And dont forget your toothbrush&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;Batman</em>
-                </small>
-              </footer>
-            </blockquote>
-             <img src="/static/github.webp" alt="Github" class="mb-5" width="400px" />
-          </v-layout>
-        </v-slide-y-transition>
-      </v-container>
-    </main>
+<v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="meetup in meetups"
+            :src="meetup.imageUrl"
+            :key="meetup.id">
+            <div class="title">
+              {{ meetup.title }}
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap  class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+       <p>Join our earth shattering meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+<script>
+export default {
+
+}
+</script>
