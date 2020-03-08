523487190c4b5ffaf2205305b412be00d7328039
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-08 19:29:10 +0000

    Add Store

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
-<v-container>
-    <v-layout row wrap class="mb-3">
+  <v-container>
+    <v-layout row wrap>
       <v-flex xs12 sm6 class="text-xs-center text-sm-right">
         <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
       </v-flex>
@@ -13,11 +13,11 @@
         <v-carousel style="cursor: pointer;">
           <v-carousel-item
             v-for="meetup in meetups"
-            v-bind:src="meetup.imageUrl"
-            v-bind:key="meetup.id"
+            :src="meetup.imageUrl"
+            :key="meetup.id"
             @click="onLoadMeetup(meetup.id)">
             <div class="title">
-            <h3 class="title" style="color:#fff;">{{ meetup.title }}</h3>
+              {{ meetup.title }}
             </div>
           </v-carousel-item>
         </v-carousel>
@@ -25,35 +25,34 @@
     </v-layout>
     <v-layout row wrap  class="mt-2">
       <v-flex xs12 class="text-xs-center">
-       <p class="font-weight-bold text--primary">Join our earth shattering and emotionally invigorating meetups!</p>
+       <p>Join our awesome and emotionally invigorating meetups!</p>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
 <script>
-export default {
-  data () {
-    return {
-      meetups: [
-        { imageUrl: './static/new-york.webp', id: 'fghstgfyer653', title: 'Meetup in New York' },
-                { imageUrl: './static/berlin.webp', id: 'hdgdtsdfrt6745', title: 'Meetup in Berlin' },
-                                { imageUrl: './static/paris.webp', id: 'hdgdtsdfrt6745', title: 'Meetup in Paris' },
-                                 { imageUrl: './static/moscow.webp', id: 'hdgdtsdfrt6745', title: 'Meetup in Moscow' },
-                                  { imageUrl: './static/amsterdam.webp', id: 'hdgdtsdfrt6745', title: 'Meetup in Amsterdam' }
-
-      ]
-
-    }
-  },
-  methods: {
-    onLoadMeetup (id) {
-      this.$router.push('/meetups/' + id)
+  export default {
+    computed: {
+      meetups () {
+        return this.$store.getters.featuredMeetups
+      }
+    },
+    methods: {
+      onLoadMeetup (id) {
+        this.$router.push('/meetups/' + id)
+      }
     }
   }
-}
-
 </script>
 
-<style lang="stylus" scoped>
-
-</style>>
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.Store({
+  state: {
+    loadedMeetups: [
+      { imageUrl: './static/new-york.webp',
+        id: 'fghstgfyer653',
+        title: 'Meetup in New York',
+        date: '2020-07-10' },
+      { imageUrl: './static/berlin.webp',
+        id: 'hdgdtsdfrt6745',
+        title: 'Meetup in Berlin',
+        date: '2020-09-10' },
+      { imageUrl: './static/paris.webp',
+        id: 'asdfkjhgfdd',
+        title: 'Meetup in Paris',
+        date: '2020-09-12' },
+      { imageUrl: './static/moscow.webp',
+        id: 'zxcvbnmfgj',
+        title: 'Meetup in Moscow',
+        date: '2020-05-05' },
+      { imageUrl: './static/amsterdam.webp',
+        id: 'qwertydfgfg',
+        title: 'Meetup in Amsterdam',
+        date: '2020-07-07' }
+
+    ]
+  },
+  user: {
+    id: 'asdfghjkldfgh',
+    registeredMeetups: ['hdgdtsdfrt6745']
+  },
+  mutations: {},
+  actions: {},
+  getters: {
+    loadedMeetups (state) {
+      return state.loadedmeetups.sort((meetupA, meetupB) => {
+        return meetupA.date > meetupB.date
+      })
+    },
+    featuredMeetups (state, getters) {
+      return getters.loadedMeetups.slice(0, 5)
+    },
+    loadedMeetup (state) {
+      return (meetupId) => {
+        return state.loadedMeetups.find((meetup) {
+          return meetup.id === meetupId
+      })
+      }
+    }
+  }
+
+})
