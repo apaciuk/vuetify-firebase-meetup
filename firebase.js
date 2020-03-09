c6ab1347c77ac5e9ad45429ffad839f5323da122
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-09 16:24:03 +0000

    Update

---
+++ firebase.js
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.10.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDQaM_FAQKogM3eJYQo--JHOyBe-RXlqeI",
+    authDomain: "vue-meetup-6aed0.firebaseapp.com",
+    databaseURL: "https://vue-meetup-6aed0.firebaseio.com",
+    projectId: "vue-meetup-6aed0",
+    storageBucket: "vue-meetup-6aed0.appspot.com",
+    messagingSenderId: "128073921799",
+    appId: "1:128073921799:web:93080727ca5f28de1cc1a2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
@@ -15,5 +16,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDQaM_FAQKogM3eJYQo--JHOyBe-RXlqeI',
+      authDomain: 'vue-meetup-6aed0.firebaseapp.com',
+      databaseURL: 'https://vue-meetup-6aed0.firebaseio.com',
+      projectId: 'vue-meetup-6aed0',
+      storageBucket: 'vue-meetup-6aed0.appspot.com'
+    })
+  }
 })
